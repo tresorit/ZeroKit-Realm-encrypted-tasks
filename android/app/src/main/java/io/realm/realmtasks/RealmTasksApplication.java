69de65f990262c0aabd7a1e91e65ed611dcdf932
Robert Angeli <angeli@tresorit.com>
2017-06-28 12:17:05 +0200

    Updated Android Realm Object Server config

--- android/app/src/main/java/io/realm/realmtasks/RealmTasksApplication.java
+++ android/app/src/main/java/io/realm/realmtasks/RealmTasksApplication.java
@@ -20,24 +20,42 @@ import android.app.Application;
 
 import com.tresorit.zerokit.AdminApi;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import io.realm.Realm;
 import io.realm.log.LogLevel;
 import io.realm.log.RealmLog;
 
 public class RealmTasksApplication extends Application {
 
-    public static final String HTTP = "http";
+    private static final Pattern patternHttp = Pattern.compile("^(https?)://(.*?)");
+
+    public static final String OBJECT_SERVER;
+    public static final String PROTOCOL;
     public static final String REALM = "realm";
+    public static final String HTTP = "http";
+
+    static {
+        Matcher matcher = patternHttp.matcher(BuildConfig.OBJECT_SERVER);
+        if (matcher.matches()) {
+            PROTOCOL = matcher.group(0);
+            OBJECT_SERVER = matcher.group(1);
+        } else {
+            PROTOCOL = HTTP;
+            OBJECT_SERVER = BuildConfig.OBJECT_SERVER;
+        }
+    }
 
     public static final String FORMAT1 = "%s://%s/%s";
     public static final String FORMAT2 = "%s://%s/%s/%s";
 
-    public static final String AUTH_URL = String.format(FORMAT1, HTTP, BuildConfig.OBJECT_SERVER, "auth");
-    public static final String REALM_URL = String.format(FORMAT2, REALM, BuildConfig.OBJECT_SERVER, "%s", "realmtasks");
-    public static final String REALM_URL_INVITES_PUBLIC = String.format(FORMAT2, REALM, BuildConfig.OBJECT_SERVER, "%s", "invites_public");
-    public static final String REALM_URL_INVITES_PRIVATE = String.format(FORMAT2, REALM, BuildConfig.OBJECT_SERVER, "%s", "invites_private");
-    public static final String REALM_URL_SHARE_PRIVATE = String.format(FORMAT2, REALM, BuildConfig.OBJECT_SERVER, "%s", "shares_private");
-    public static final String REALM_URL_PERMISSION = String.format(FORMAT2, REALM, BuildConfig.OBJECT_SERVER, "%s", "__permission");
+    public static final String AUTH_URL = String.format(FORMAT1, PROTOCOL, OBJECT_SERVER, "auth");
+    public static final String REALM_URL = String.format(FORMAT2, REALM, OBJECT_SERVER, "%s", "realmtasks");
+    public static final String REALM_URL_INVITES_PUBLIC = String.format(FORMAT2, REALM, OBJECT_SERVER, "%s", "invites_public");
+    public static final String REALM_URL_INVITES_PRIVATE = String.format(FORMAT2, REALM, OBJECT_SERVER, "%s", "invites_private");
+    public static final String REALM_URL_SHARE_PRIVATE = String.format(FORMAT2, REALM, OBJECT_SERVER, "%s", "shares_private");
+    public static final String REALM_URL_PERMISSION = String.format(FORMAT2, REALM, OBJECT_SERVER, "%s", "__permission");
 
     public static final String REALM_URL_MY = String.format(REALM_URL, "~");
     public static final String REALM_URL_INVITES_PUBLIC_MY = String.format(REALM_URL_INVITES_PUBLIC, "~");
